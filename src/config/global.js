6425db7d7b4b1e22ca7d1eb6f21d158c4e8efcac
cveimarsena <vcelis@sena.edu.co>
2026-06-17 06:11:24 -0500

    Final

--- src/config/global.js
+++ src/config/global.js
@@ -190,7 +190,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CFA1_33130209_DU.pdf',
       },
       {
         icono: 'fas fa-download',
